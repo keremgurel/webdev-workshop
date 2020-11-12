1ebf2c637870bda29f4151475d7a259b0e2440ad
Kerem Gurel <keremgurell99@gmail.com>
2020-11-12 00:39:37 -0800

    reformatted header

--- webdev-workshop-ui/src/components/header/Header.js
+++ webdev-workshop-ui/src/components/header/Header.js
@@ -4,6 +4,7 @@ import DehazeIcon from '@material-ui/icons/Dehaze';
 import SearchIcon from "@material-ui/icons/Search";
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 import logo from "../src/Images/fanjoy.png";
 
 function Header() {
@@ -21,19 +22,20 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLine">CAD$</span>
         </div>
+        <ExpandMoreOutlinedIcon fontSize="small" className="header__expandIcon" />
 
         <div className="header__option">
-          <PersonOutlineOutlinedIcon />
+          <PersonOutlineOutlinedIcon  className="header__accountIcon"/>
         </div>
 
         <div className="header__option">
-          <SearchIcon />
+          <SearchIcon className="header__searchIcon" />
         </div>
 
-        <div className="header__optionBasket">
-            <LocalMallOutlinedIcon />
-            {/* <span className="header_optionLine header__basketCount">0</span> */}
-        </div>
+          <div className="header__option">
+            <LocalMallOutlinedIcon className="header__BasketIcon"/>
+            {/* <span className="header_optionLineTwo header__basketCount">0</span> */}
+          </div>
       </div>
     </div>
   );
