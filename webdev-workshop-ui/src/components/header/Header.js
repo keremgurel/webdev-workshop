f5f8d61caa10bc26bc22210dea43031c4387019a
Kerem Gurel <keremgurell99@gmail.com>
2020-11-11 21:54:03 -0800

    Header first commit

--- webdev-workshop-ui/src/components/header/Header.js
+++ webdev-workshop-ui/src/components/header/Header.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React from 'react'
+import './Header.css'
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
+import logo from "../src/Images/fanjoy.png";
 
-const Header = () => {
-  return (
-    null;
-  )
+function Header() {
+    return (
+    <div className="header">
+      <div className="header__dehaze">
+        <DehazeIcon className="header__dehazeIcon" />
+        {/* Logo */}
+      </div>
+
+      <div>
+      <img className="header__logo" src={logo} alt="" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLine">CAD$</span>
+        </div>
+
+        <div className="header__option">
+          <PersonOutlineOutlinedIcon />
+        </div>
+
+        <div className="header__option">
+          <SearchIcon />
+        </div>
+
+          <div className="header__optionBasket">
+            <LocalMallOutlinedIcon />
+            {/* <span className="header_optionLine header__basketCount">0</span> */}
+          </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
